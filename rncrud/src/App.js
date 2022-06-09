1b5b9036e4ed50dbfc3b1064e2fdd96877080d08
verenaribeiro67 <verenaribeiro67@gmail.com>
2022-06-09 16:02:54 -0300

    Ajustes e inserções

--- rncrud/src/App.js
+++ rncrud/src/App.js
@@ -1,63 +1,57 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
 import { Button, Icon } from 'react-native-elements'
-import {TouchableOpacity, Text } from "react-native";
+import { UsersProvider } from './context/UsersContext'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 
 const Stack = createNativeStackNavigator()
 
 export default props => {
     return (
-        <NavigationContainer>
-
-            <Stack.Navigator
-                initialRouteName="Userlist"
-                screenOptions={screenOptions}>
-
-                <Stack.Screen
-                    name="UserList"
-                    component={UserList}
-                    options={() => {
-                        return{
-                           title: "Lista de Usuários",
-                            headerLeft: () => {
-                               <Text>Olá</Text>
-                            },
-                            headerRight: () => (
-
-                                <Button
-                                type="clear"
-                                icon={<Icon name="add" size={25} color="#FF0"/>}
-                               />
-                            )
-                        }
-                    }}
-                />
-
-                <Stack.Screen
-                    name="UserForm"
-                    component={UserForm}
-                    options={{
-                        title: "Formulário de Usuários"
-                    }}
-                />
-
-            </Stack.Navigator>
-
-        </NavigationContainer>
+        <UsersProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="UserList"
+                    screenOptions={screenOptions}>
+                    <Stack.Screen
+                        name="UserList"
+                        component={UserList}
+                        options={({ navigation }) => {
+                            return {
+                                title: "Lista de Usuários",
+                                headerRight: () => (
+                                    <Button
+                                        onPress={() => navigation.navigate("UserForm")}
+                                        type="clear"
+                                        icon={<Icon name="add" size={25} color="white" />}
+                                    />
+                                )
+                            }
+                        }}
+                    />
+                    <Stack.Screen
+                        name="UserForm"
+                        component={UserForm}
+                        options={{
+                            title: "Formulário de Usuários"
+                        }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </UsersProvider>
     )
 }
 
 const screenOptions = {
     headerStyle: {
-        backgroundColor: '#993399'
+        backgroundColor: '#f4511e'
     },
     headerTintColor: '#fff',
     headerTitleStyle: {
-        fontWeight: 'bold',
+        fontWeight: 'bold'
     }
-
-
 }
---
+++ rncrud/src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'Verena Queiroz',
+        email: 'verenaribeiro67@gmail.com',
+        avatarUrl:
+            'https://media-exp2.licdn.com/dms/image/C4D03AQEoA8jmDQRurA/profile-displayphoto-shrink_100_100/0/1614793888992?e=1660176000&v=beta&t=q6mRKMAPKNsslWrp-vTQKhMsw2pTZTkGjnHURIwfyA0',
+    },
+    {
+        id: 2,
+        name: 'Luiz Queiroz',
+        email: 'riamasil@empmail.com',
+        avatarUrl:
+            'https://media-exp2.licdn.com/dms/image/C4E03AQH-7zU8CnZXnA/profile-displayphoto-shrink_200_200/0/1590503314853?e=1660176000&v=beta&t=qeBDT93k6FINyJAJWCOKzPYRkgtzYyhEV_yCzJ3dvRc',
+    },
+    {
+        id: 3,
+        name: 'Douglas Lazarin',
+        email: 'douglas.lazarin@ixcsoft.com.br',
+        avatarUrl:
+            'https://media-exp2.licdn.com/dms/image/C4E03AQHw9OSt5aBHWg/profile-displayphoto-shrink_100_100/0/1619696224227?e=1660176000&v=beta&t=CnBpzhov04-s5fzo5Fu9aDCRBM7Q3oBYl5r9zzFiWkQ',
+    },
+    {
+        id: 4,
+        name: 'Tales Born',
+        email: 'tales.born@ixcsoft.com.br',
+        avatarUrl:
+            'https://media-exp2.licdn.com/dms/image/C4D03AQEWYdg7M_KWnQ/profile-displayphoto-shrink_100_100/0/1545659772134?e=1660176000&v=beta&t=GgeHU51WtTsQ_5tL3EwJgvhWM1axlq7RnLoa9k3dGcw',
+    },
+];
