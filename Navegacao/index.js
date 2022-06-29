1305fdc2aa1fd664edbc51b97b9aaad7fedec4e7
Verena Cavalcante <36251054+verenaribeiro67@users.noreply.github.com>
2022-06-29 10:01:39 -0300

    Delete Navegacao directory

--- Navegacao/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Navegacao from './src/navegacao';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Navegacao);
--- Navegacao/src/views/TelaA.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TextoCentral from '../components/TextoCentral'
-
-export default props => (
-    <TextoCentral corFundo='#e53935'>
-        Tela A
-    </TextoCentral>
-)
